3d99786cbed6942148b7a06542525c6e5893f258
iguana_man <ryanleelambert@gmail.com>
2013-11-28 10:37:36 +0900

    Version 1o release
    Made compatible with latest stable Tinkers Construct

--- src/iguanaman/iguanatweakstconstruct/util/IguanaToolBuildRecipe.java
+++ src/iguanaman/iguanatweakstconstruct/util/IguanaToolBuildRecipe.java
@@ -81,7 +81,7 @@ public class IguanaToolBuildRecipe implements IRecipe {
                 		if (IguanaConfig.easyToolCreation && !toolFound) 
                     		output = ToolBuilder.instance.buildTool(input1, input2, input3, input4, "");
                 		else if (input1.getItem() instanceof ToolCore)
-                    		output = ToolBuilder.instance.modifyTool(input1, input2, input3, input4);
+                    		output = ToolBuilder.instance.modifyTool(input1, input2, input3, input4, "");
                         
                         // If modification was successful
                         if (output != null) return true;

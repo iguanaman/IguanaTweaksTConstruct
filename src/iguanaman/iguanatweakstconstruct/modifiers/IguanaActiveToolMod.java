0dd3eb97d8f4d8b65971de80eb2d70c12f2a245d
iguana_man <ryanleelambert@gmail.com>
2013-12-20 14:49:03 +0900

    Fixed certain hammers not gaining xp

--- src/iguanaman/iguanatweakstconstruct/modifiers/IguanaActiveToolMod.java
+++ src/iguanaman/iguanatweakstconstruct/modifiers/IguanaActiveToolMod.java
@@ -55,14 +55,33 @@ public class IguanaActiveToolMod extends ActiveToolMod {
         int bID = entity.worldObj.getBlockId(x, y, z);
         int meta = entity.worldObj.getBlockMetadata(x, y, z);
         Block block = Block.blocksList[bID];
-        
+    	
         if (block == null || materialBlacklist.contains(block.blockMaterial)) return false;
+
+		NBTTagCompound tags = stack.getTagCompound().getCompoundTag("InfiTool");	
+
+        int miningSpeed = tags.hasKey("MiningSpeed") ? tags.getInteger("MiningSpeed"): -1;
+        int miningSpeed2 = tags.hasKey("MiningSpeed2") ? tags.getInteger("MiningSpeed2"): -1;
+        int miningSpeedHandle = tags.hasKey("MiningSpeedHandle") ? tags.getInteger("MiningSpeedHandle"): -1;
+        int miningSpeedExtra = tags.hasKey("MiningSpeedExtra") ? tags.getInteger("MiningSpeedExtra"): -1;
+
+        if (miningSpeed > 0) tags.setInteger("MiningSpeed", miningSpeed * 2);
+        if (miningSpeed2 > 0) tags.setInteger("MiningSpeed2", miningSpeed2 * 2);
+        if (miningSpeedHandle > 0) tags.setInteger("MiningSpeedHandle", miningSpeedHandle * 2);
+        if (miningSpeedExtra > 0) tags.setInteger("MiningSpeedExtra", miningSpeedExtra * 2);
+
+    	//IguanaLog.log(tool.canHarvestBlock(block) + " " + Float.toString(tool.getStrVsBlock(stack, block, meta)));
         
         if (tool.canHarvestBlock(block) && tool.getStrVsBlock(stack, block, meta) > 1f) 
         {
         	//IguanaLog.log("xp added");
         	IguanaLevelingLogic.addXP(stack, (EntityPlayer)entity, 1L);
         }
+
+        if (miningSpeed > 0) tags.setInteger("MiningSpeed", miningSpeed);
+        if (miningSpeed2 > 0) tags.setInteger("MiningSpeed2", miningSpeed2);
+        if (miningSpeedHandle > 0) tags.setInteger("MiningSpeedHandle", miningSpeedHandle);
+        if (miningSpeedExtra > 0) tags.setInteger("MiningSpeedExtra", miningSpeedExtra);
         
     	return false;
     }

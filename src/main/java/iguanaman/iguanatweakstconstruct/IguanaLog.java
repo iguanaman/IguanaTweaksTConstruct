412322f655bfc141ab88fde4695216eed37b1047
Progwml6 <progwml6@gmail.com>
2014-05-03 13:44:59 -0400

    misc updates & gradleization

---
+++ src/main/java/iguanaman/iguanatweakstconstruct/IguanaLog.java
@@ -0,0 +1,11 @@
+package iguanaman.iguanatweakstconstruct;
+
+import static iguanaman.iguanatweakstconstruct.IguanaTweaksTConstruct.ITconTweaksLog;
+public class IguanaLog {
+
+	public static void log(String text)
+	{
+		ITconTweaksLog.info(text);
+	}
+
+}

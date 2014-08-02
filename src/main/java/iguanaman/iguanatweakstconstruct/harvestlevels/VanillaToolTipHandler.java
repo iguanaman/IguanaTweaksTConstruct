40c8f318f6f00409d9828088ee1f3f7fa33f4bb9
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2014-08-02 14:26:28 +0200

    Fix mining level tooltips being displayed on EVERYTHING applicable instead of just vanilla tools :S (like, ticon tools .)

--- src/main/java/iguanaman/iguanatweakstconstruct/harvestlevels/VanillaToolTipHandler.java
+++ src/main/java/iguanaman/iguanatweakstconstruct/harvestlevels/VanillaToolTipHandler.java
@@ -3,6 +3,7 @@ package iguanaman.iguanatweakstconstruct.harvestlevels;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import iguanaman.iguanatweakstconstruct.leveling.LevelingTooltips;
 import net.minecraft.item.ItemPickaxe;
+import net.minecraft.item.ItemTool;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import tconstruct.library.tools.ToolCore;
 
@@ -13,11 +14,23 @@ public class VanillaToolTipHandler {
         if (event.entityPlayer == null)
             return;
 
-        // we're only interested in stuff that's basically a pickaxe
-        int hlvl = event.itemStack.getItem().getHarvestLevel(event.itemStack, "pickaxe");
-        if (hlvl < 0)
+        if(!(event.itemStack.getItem() instanceof ItemTool))
             return;
 
-        event.toolTip.add(1, LevelingTooltips.getMiningLevelTooltip(hlvl));
+        // we're only interested in stuff that's basically a pickaxe
+        int hlvl = event.itemStack.getItem().getHarvestLevel(event.itemStack, "pickaxe");
+        if (hlvl >= 0)
+            event.toolTip.add(1, LevelingTooltips.getMiningLevelTooltip(hlvl));
+
+        // well.. let's check the other things too /o\
+        /* disabled because it'll probably cause more confusion than help..
+        hlvl = event.itemStack.getItem().getHarvestLevel(event.itemStack, "shovel");
+        if (hlvl >= 0)
+            event.toolTip.add(1, LevelingTooltips.getMiningLevelTooltip(hlvl));
+
+        hlvl = event.itemStack.getItem().getHarvestLevel(event.itemStack, "axe");
+        if (hlvl >= 0)
+            event.toolTip.add(1, LevelingTooltips.getMiningLevelTooltip(hlvl));
+        */
     }
 }

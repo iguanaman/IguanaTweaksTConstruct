590b1a2354cca530fa1b921a009adb458d52a178
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2014-08-14 20:24:11 +0200

    Rename modules a bit

--- src/main/java/iguanaman/iguanatweakstconstruct/reference/Reference.java
+++ src/main/java/iguanaman/iguanatweakstconstruct/reference/Reference.java
@@ -11,10 +11,10 @@ public class Reference {
     public static final String PULSE_HARVESTTWEAKS = "HarvestLevelTweaks";
     public static final String PULSE_REPLACING = "ToolPartReplacing";
     public static final String PULSE_MOBHEADS = "MobHeads";
-    public static final String PULSE_TWEAKS = "IguanaTweaks";
-    public static final String PULSE_ITEMS = "IguanaItems";
-    public static final String PULSE_RESTRICTIONS = "IguanaRestrictions";
-    public static final String PULSE_WORLDGEN = "IguanaWorldGen";
+    public static final String PULSE_TWEAKS = "Tweaks";
+    public static final String PULSE_ITEMS = "Items";
+    public static final String PULSE_RESTRICTIONS = "Restrictions";
+    public static final String PULSE_WORLDGEN = "WorldGen";
 
     public static final String PROXY_CLIENT_CLASS = "iguanaman.iguanatweakstconstruct.proxy.ClientProxy";
     public static final String PROXY_SERVER_CLASS = "iguanaman.iguanatweakstconstruct.proxy.ServerProxy";

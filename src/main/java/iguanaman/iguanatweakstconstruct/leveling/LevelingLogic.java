176bc060a098a9ba211eb5564139a287bc93300f
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2014-12-23 16:26:14 +0100

    Fix weapons with attack=0 materials having invalid xp

--- src/main/java/iguanaman/iguanatweakstconstruct/leveling/LevelingLogic.java
+++ src/main/java/iguanaman/iguanatweakstconstruct/leveling/LevelingLogic.java
@@ -203,7 +203,7 @@ public final class LevelingLogic {
 		{
             base = 140f;
             base *= ((ToolCore)tool.getItem()).getDamageModifier();
-			base *= tags.getInteger("Attack") * 1.2f;
+			base *= Math.max(1, tags.getInteger("Attack")) * 1.2f;
 
 			if (tool.getItem() instanceof Scythe) base *= 1.5f;
 			base *= Config.xpRequiredWeaponsPercentage / 100f;
